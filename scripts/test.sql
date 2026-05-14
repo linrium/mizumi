e453c2cf84660f789fe1481f1ff8a5de77cfefd2
linrium <linrium@gmail.com>
2026-05-14 11:15:00 +0900

    fix: uc bootstrap

---
+++ scripts/test.sql
@@ -0,0 +1,9 @@
+LOAD unity_catalog;
+CREATE SECRET (
+     TYPE     unity_catalog,
+     TOKEN    'not-used',
+     ENDPOINT 'http://localhost:8082'
+);
+ATTACH 'hdbank' AS hdbank (TYPE unity_catalog, DEFAULT_SCHEMA 'hdbank_payments_prod_bronze');
+ATTACH 'vietjetair' AS vietjetair (TYPE unity_catalog, DEFAULT_SCHEMA 'vietjetair_bookings_prod_bronze');
+SHOW ALL TABLES;
